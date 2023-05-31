eba45abac5099b95a507b3f7812508c915f553c5
oyupanqui <osmar30asis@gmail.com>
2023-05-30 23:29:03 -0500

    added initial js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-function openForm() {
-    alert("Hello there!")
-}
+const form = document.getElementById("form")
+
+console.log(form)
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+})
